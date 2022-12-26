0d3ffe05832ace7fa9aa87ff3231e2a19163e091
Spencillian <spencerli253@gmail.com>
2022-12-26 04:14:56 -0500

    day 2 p1

--- day2p1.c
+++ day2p1.c
@@ -7,11 +7,43 @@ int main(){
 
     fp = fopen("day2.txt", "r");
 
+    int total = 0;
     while (fgets(buff, 255, fp)){
+        int hand;
+        if(buff[0] == 'A'){
+            hand = 1;
+        }else if (buff[0] == 'B') {
+            hand = 2;
+        }else {
+            hand = 3;
+        }
+        
+        int response;
+        if(buff[2] == 'X'){
+            response = 1;
+        }else if (buff[2] == 'Y') {
+            response = 2;
+        }else {
+            response = 3;
+        }
+
+        int game_results = (hand - response + 2) % 3;
+
+        int score;
+        if (game_results == 2) {
+            score = 3 + response;
+        }else if (game_results == 0) {
+            score = response;
+        }else {
+            score = 6 + response;
+        }
+
+        total += score;
         printf("%s", buff);
     }
-
     fclose(fp);
+
+    printf("%d", total);
 
     return 0;
 }
