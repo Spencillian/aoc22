59329d32e4e9029f987431a51d146b99c824222e
Spencillian <spencerli253@gmail.com>
2022-12-28 09:16:02 -0500

    learned how to write a stack in c

--- day5p1.c
+++ day5p1.c
@@ -1,13 +1,62 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    size_t top;
+    char arr[255];
+} stack;
+
+stack init_stack(char array[]){
+    stack s = {
+        .top = strlen(array),
+    };
+    strcpy(s.arr, array);
+    return s;
+}
+
+void push(stack *s, char label){
+    s->arr[s->top++] = label;
+}
+
+char pop(stack *s){
+    return s->arr[s->top--];
+}
+
+//
+//[M]                     [N] [Z]
+//[F]             [R] [Z] [C] [C]
+//[C]     [V]     [L] [N] [G] [V]
+//[W]     [L]     [T] [H] [V] [F] [H]
+//[T]     [T] [W] [F] [B] [P] [J] [L]
+//[D] [L] [H] [J] [C] [G] [S] [R] [M]
+//[L] [B] [C] [P] [S] [D] [M] [Q] [P]
+//[B] [N] [J] [S] [Z] [W] [F] [W] [R]
+// 1   2   3   4   5   6   7   8   9
 
 int main(){
     FILE *fp;
     char buff[255];
 
-    fp = fopen("input.txt", "r");
+    fp = fopen("day5.txt", "r");
+
+    stack stacks[] = {
+        init_stack("MFCWTDLB"),
+        init_stack("LBN"),
+        init_stack("VLTHCJ"),
+        init_stack("WJPS"),
+        init_stack("RLTFCSZ"),
+        init_stack("ZNHBGDW"),
+        init_stack("NCGVPSMF"),
+        init_stack("ZCVFJRQW"),
+        init_stack("HLMPR"),
+    };
 
     while (fgets(buff, 255, fp) != NULL) {
+        if (buff[0] != 'm') {
+            continue;
+        }
         printf("%s", buff);
     }
 
--- test.c
+++ test.c
@@ -1,17 +1,37 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    size_t top;
+    char arr[255];
+} stack;
+
+stack init_stack(char array[]){
+    stack s = {
+        .top = strlen(array),
+    };
+    strcpy(s.arr, array);
+    return s;
+}
+
+void push(stack *s, char label){
+    s->arr[s->top++] = label;
+}
 
 int main(){
-    char val = '\0';
-    
-    int result = (int)(val - 'A' + 1);
-    if(val >= 'a'){
-        result = val - 'a' + 1;
-    }else{
-        result = val - 'A' + 26 + 1;
-    }
-    printf("%c\n", val);
-    printf("%d\n", result);
+    stack s = init_stack("HELLO");
+    stack t = init_stack("HELLO");
+
+    printf("%s\n", s.arr);
+
+    push(&s, '!');
+    printf("%s\n", s.arr);
+    printf("%s\n", t.arr);
+
+    push(&s, '6');
+    printf("%s\n", s.arr);
 
     return 0;
 }
